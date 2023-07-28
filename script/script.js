32f3625601674b8fe5fcadbec96cc6456f4eab16
soyeon <sysy0313@gmail.com>
2023-07-28 17:37:56 +0900

    swiper slide

--- script/script.js
+++ script/script.js
@@ -27,3 +27,32 @@ mNav.addEventListener('click', function(){ //햄버거 메뉴 클릭시,(객체.
 closeBtn.addEventListener('click', function(){ //닫기 버튼을 클릭했을 때
     mNavOpen.style.display = 'none'; //모바일 메뉴를 숨긴다
 })
+//main-swiper-slide
+//const 변수명 = new Swiper('적용대상');
+//const 변수명 = new Swiper('적용대상,{속성:값, 속성:값}');
+const pawin_slide = new Swiper('#pawin_slide',{
+    autoplay:{
+        delay:1000,
+        disableOnInteraction:false,//버튼 클릭 후 자동재생유지
+    }, //자동재생
+    loop:true, //마지막슬라이드->첫슬라이드 자연스럽게
+    // direction:'vertical'
+    effect:'fade', //제자리 자연스러운 변경 슬라이드 효과
+    navigation: { //이전, 다음 네이게이션 연결
+        // next, prev 객체 연결 시 부모를 안 적으면
+        // body 안에 있는 모든 swiper-next,prev를 인식하기 때문에
+        // 개별인식 가능한 부모이름을 반드시 앞에 먼저 작성한다.
+        nextEl: '#pawin_slide .swiper-button-next',
+        prevEl: '#pawin_slide .swiper-button-prev',
+    },
+});
+const my_slide = new Swiper('#my_slide',{
+    autoplay:{delay:1000},
+    loop:true,
+    direction:'vertical',//반드시 따옴표 붙이기~!
+    navigation: { 
+        nextEl: '#my_slide .swiper-button-next',
+        prevEl: '#my_slide .swiper-button-prev',
+    },
+    
+})
